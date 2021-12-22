52ea367db66a4db21c56dbf19ef6a5f5b64c82f4
JohnBernad <75006549+JohnBernad@users.noreply.github.com>
2021-12-22 14:51:44 +0530

    Update mul.py

--- mul.py
+++ mul.py
@@ -1,5 +1,5 @@
 def mul():
-   num1 = float(input("Enter first number : "))
+     num1 = float(input("Enter first number : "))
      num2 = float(input("Enter second number : "))
      p=num1*num2
      print("the product is",p)
