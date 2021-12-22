2eec603ce75fed365fac60c3f6ec9b8239bfd0d0
JohnBernad <75006549+JohnBernad@users.noreply.github.com>
2021-12-22 14:51:11 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -3,4 +3,4 @@ def sub():
      num1 = float(input("Enter first number: "))
      num2 = float(input("Enter second number: "))
      sub=num1-num2
-print("The difference is", sub)
+     print("The difference is", sub)
