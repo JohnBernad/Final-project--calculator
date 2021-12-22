dbb31e6a618f40fbb8d97859ca9217a2fb0714ac
ash <ash@xyz.com>
2021-12-22 14:29:59 +0530

    Create main (1).py

---
+++ main (1).py
@@ -0,0 +1,14 @@
+#Main part
+
+ch=int(input("Enter your choice\n 1.Adition 2. Subtraction 3.Multiply 4.Division)
+if(ch==1):
+     add()
+
+if(ch==2):
+     sub()
+
+if(ch==3):
+     mul()
+
+if(ch==4):
+     div()
