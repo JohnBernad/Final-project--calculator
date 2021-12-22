71d440b715085a9edb2263ef1fa0fafdc2817381
ash <ash@xyz.com>
2021-12-22 14:23:37 +0530

    sonimas version
    sonimas version

---
+++ sub.py
@@ -0,0 +1,6 @@
+#This is for subtraction
+def sub():
+     num1 = float(input("Enter first number: "))
+     num2 = float(input("Enter second number: "))
+     sub=num1-num2
+print("The difference is", sub)
